b69cb67436d3b9bf84236693374323698d63ddb6
Ilofrudd <boyko5000@gmail.com>
2024-02-12 17:15:32 +0100

    lesson12

---
+++ src/lesson12/HomeWork12.java
@@ -0,0 +1,75 @@
+package lesson12;
+
+import java.util.Random;
+import java.util.Arrays;
+public class HomeWork12 {
+    public static void main(String[] args) {
+        int[] array = generateRandomArray(10, 1, 100);
+        System.out.println("Massive: " + Arrays.toString(array));
+
+        //Task1
+        System.out.println(" |Task1| ");
+        int minPosition = linearSearchMin(array);
+        System.out.println("Min element minPosition  " + minPosition);
+
+        //Task2
+
+        System.out.println(" |Task2| ");
+        selectionSort(array);
+        System.out.println(Arrays.toString(array));
+
+        //Task3
+
+        System.out.println(" |Task3| ");
+        int maxPosition = binarySearchMax(array);
+        System.out.println("Max element position (binary search): " + maxPosition);
+    }
+    static int[] generateRandomArray(int size, int min, int max) {
+        int[] array = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(max - min + 1) + min;
+        }
+        return array;
+    }
+    static int linearSearchMin(int[] array) {
+        int minIdx = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[minIdx]) {
+                minIdx = i;
+            }
+        }
+        return minIdx;
+    }
+    static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = array[i + 1];
+            int minIdx = i + 1;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minIdx = j;
+
+                }
+            }
+            if (array[i] > min) {
+                array[minIdx] = array[i];
+                array[i] = min;
+            }
+        }
+    }
+    static int binarySearchMax(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] > array[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
+
---
+++ src/lesson12/Lesson12.java
@@ -0,0 +1,49 @@
+package lesson12;
+
+import java.util.Arrays;
+
+public class Lesson12 {
+    public static void main(String[] args) {
+        int[] array = {1, 8, 12, -4, 6, 3, 7, -13};
+        int toSearch = -0;
+        int idx = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == toSearch) {
+                idx = 1;
+                break;
+            }
+        }
+        System.out.println(idx);
+        System.out.println(Arrays.toString(array));
+        selectionSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+    static void exchangeSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    int tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                }
+            }
+        }
+    }
+    static void selectionSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            int min = a[i + 1];
+            int minIdx = i + 1;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < min) {
+                    min = a[j];
+                    minIdx = j;
+
+                }
+            }
+            if (a[i] > min) {
+                a[minIdx] = a[i];
+                a[i] = min;
+            }
+        }
+    }
+}
