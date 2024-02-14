d9061f596fbc0ebfbc07ebbeb9008ad08b13351e
Ilofrudd <boyko5000@gmail.com>
2024-02-14 19:04:13 +0100

    Update HomeWork13.java

--- src/lesson13/HomeWork13.java
+++ src/lesson13/HomeWork13.java
@@ -9,11 +9,13 @@ import lesson12.HomeWork12;
  */
 public class HomeWork13 {
     public static void main(String[] args) {
+
         // Task #1
+
         long startTime = System.currentTimeMillis();
         //int[] array = HomeWork12.generateRandomArray(100_000, -100, 100);
         int[] array = HomeWork12.generateRandomArray(100_000, -100, 100);
-        System.out.println((System.currentTimeMillis() - startTime) + " ms - create array");
+        System.out.println((System.currentTimeMillis() - startTime) + " ms - generate array");
 
         startTime = System.currentTimeMillis();
         HomeWork12.linearSearch(array, 37);
