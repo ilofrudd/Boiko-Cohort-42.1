8b6a21a9a05cb358254d4703a7d4da2333a3e7f6
Ilofrudd <boyko5000@gmail.com>
2024-03-01 22:33:16 +0100

    l21

---
+++ src/extend/Interpreter.java
@@ -0,0 +1,36 @@
+package extend;
+
+import java.util.Scanner;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, hw #10 ext
+ *
+ * @author Boiko Yaroslav
+ * @version 28.02.2024
+ */
+public class Interpreter {
+
+    public static void main(String[] args) {
+        Variables variables = new Variables();
+        Scanner scanner = new Scanner(System.in);
+        String line;
+        do {
+            System.out.print("# ");
+            line = scanner.nextLine();
+            String[] tokens = line.split(" ");
+            switch (tokens[0]) {
+                case "print":
+                    Integer value = variables.getValue(tokens[1]);
+                    if (value != null) {
+                        System.out.println(value);
+                    }
+                    break;
+                case "exit":
+                    System.out.println("Exit from interpreter.");
+                    break;
+                default:
+                    variables.assignValue(line);
+            }
+        } while (!line.equals("exit"));
+    }
+}
--- src/lesson19/RubberArray.java
+++ src/lesson19/RubberArray.java
@@ -6,7 +6,7 @@ public class RubberArray {
     private final int INIT_DATA_SIZE = 10;
     private int[] data;
     private int length;
-    
+
     public RubberArray() {
         data = new int[INIT_DATA_SIZE];
         length = 0;
